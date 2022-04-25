752e825fd854659d4a6aef103da25adb4389dfa2
Pedro Oliveira <phr.oliveira08@gmail.com>
2022-04-25 19:28:01 -0300

    first commit

---
+++ guessing_game/guessinggame.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "**************************************" << endl;
+  cout << "**** Welcome to the Guessing Game ****" << endl;
+  cout << "**************************************" << endl;
+}
