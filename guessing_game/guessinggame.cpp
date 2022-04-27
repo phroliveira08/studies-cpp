c651a2000fa4a1666de54b7cac9944d9660493a0
Pedro Oliveira <phr.oliveira08@gmail.com>
2022-04-26 21:15:48 -0300

    v1.0 Guessing Game

--- guessing_game/guessinggame.cpp
+++ guessing_game/guessinggame.cpp
@@ -1,21 +1,73 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-int secretNumber = 10;
-int isRunning = true;
+bool isRunning = true;
+
+const int EASY = 15;
+const int MEDIUM = 10;
+const int HARD = 5;
+
+double score = 1000.0;
+int attempts = 0;
+int limitAttempts = 15;
+
+void setDifficulty()
+{
+  cout << "Choose a difficulty, E (Easy)/M (Medium)/H (Hard)" << endl;
+  char gameDifficulty;
+  cin >> gameDifficulty;
+  switch (gameDifficulty)
+  {
+  case 'E':
+    limitAttempts = EASY;
+    break;
+  case 'M':
+    limitAttempts = MEDIUM;
+    break;
+  case 'H':
+    limitAttempts = HARD;
+    break;
+  default:
+    limitAttempts = EASY;
+    cout << "It mode doesn't exist, set default mode Easy" << endl;
+    break;
+  }
+}
+
+void rulesToEndGame()
+{
+  int fixedLimitAttempts = limitAttempts + 1;
+  if (limitAttempts == attempts)
+  {
+    isRunning = false;
+    cout << "You lost the game because the attempt limit are reached, try again in other difficulty mode" << endl;
+  }
+}
 
 void game()
 {
+  srand(time(NULL));
+  const int SECRET_NUMBER = rand() % 100;
+  cout << SECRET_NUMBER << endl;
   int guestNumber;
-  cout << "Try to guess the secret number" << endl;
+  setDifficulty();
+  cout << "Try to guess the secret number between 0 and 99" << endl;
   while (isRunning)
   {
+    attempts++;
+    cout << "Attempt: " << attempts << endl;
     cin >> guestNumber;
-    bool isValid = guestNumber == secretNumber;
-    bool isHigher = guestNumber > secretNumber;
+    bool isValid = guestNumber == SECRET_NUMBER;
+    bool isHigher = guestNumber > SECRET_NUMBER;
+    score = score - abs(SECRET_NUMBER - guestNumber) / 2.0;
     if (isValid)
     {
-      cout << "You win!" << endl;
+      cout << "You win in the " << attempts << " attempt!" << endl;
+      cout.precision(2);
+      cout << fixed;
+      cout << "Your score: " << score << endl;
       isRunning = false;
     }
     else if (isHigher)
@@ -26,6 +78,7 @@ void game()
     {
       cout << "This number is less than secret number" << endl;
     }
+    rulesToEndGame();
   }
 }
 
