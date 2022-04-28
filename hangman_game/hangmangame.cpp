fe76b995bc8c9a75ed3f62e92d629bc7b6165fca
Pedro Oliveira <phr.oliveira08@gmail.com>
2022-04-27 22:53:02 -0300

    introduction hangman game

---
+++ hangman_game/hangmangame.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "*********************************" << endl;
+  cout << "**** Welcome to Hangman Game ****" << endl;
+  cout << "*********************************" << endl;
+}
