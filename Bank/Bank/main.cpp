b242b7cd9983ff3c305583b5c569297649aa25b9
Pedro Oliveira <phr.oliveira08@gmail.com>
2022-05-21 23:52:08 -0300

    test error

--- Bank/Bank/main.cpp
+++ Bank/Bank/main.cpp
@@ -10,12 +10,18 @@ void showBalance(const Account& account) {
 int main() {
 	Account account1("Nome1", "100");
 	Account account2("Nome2", "101");
+	Account account3("Nome3", "102");
+	Account account4("Nome4", "103");
 	
 	account1.deposit(500);
 	account1.withdraw(200);
 	account1.transferTo(account2, 140);
 	account2.transferTo(account1, 40);
+	account1.transferTo(account3, 20);
+	account1.transferTo(account4, -25);
 
 	showBalance(account1);
 	showBalance(account2);
+	showBalance(account3);
+	showBalance(account4);
 }
