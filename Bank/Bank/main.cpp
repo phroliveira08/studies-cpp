594bdb91f0dfb8b879bc3d7aa1386e2e8c596ef2
Pedro Oliveira <phr.oliveira08@gmail.com>
2022-05-26 22:34:32 -0300

    create class in heap and overload a function

--- Bank/Bank/main.cpp
+++ Bank/Bank/main.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 
+void showBalance(const Account* account) {
+	cout << "Balance from " << account->getProfileName() << " is " << account->getBalance() << endl;
+}
 void showBalance(const Account& account) {
 	cout << "Balance from " << account.getProfileName() << " is " << account.getBalance() << endl;
 }
@@ -12,6 +15,8 @@ int main() {
 	Account account2("Nome2", "101");
 	Account account3("Nome3", "102");
 	Account account4("Nome4", "103");
+
+	Account* account5 = new Account("Nome5", "103"); //HEAP
 	
 	account1.deposit(800);
 	account1.withdraw(200);
@@ -19,11 +24,13 @@ int main() {
 	account2.transferTo(account1, 40);
 	account1.transferTo(account3, 20);
 	account1.transferTo(account4, 25);
+	account5->deposit(1000);
 
 	showBalance(account1);
 	showBalance(account2);
 	showBalance(account3);
 	showBalance(account4);
+	showBalance(account5);
 
 	cout << "Num: " << Account::getNumberAccounts() << endl;
 }
